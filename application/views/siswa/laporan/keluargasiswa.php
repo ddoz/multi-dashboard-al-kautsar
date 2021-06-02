0dfcb95855d2c94ac25f53c24344d7d0b7389398
Adam Japal <adam.japal@gmail.com>
2021-06-02 11:13:35 +0700

    a

--- application/views/siswa/laporan/keluargasiswa.php
+++ application/views/siswa/laporan/keluargasiswa.php
@@ -20,7 +20,7 @@
                                             <select name="ta" required class="form-control">
                                                 <option value="">Pilih Tahun Akademik</option>
                                                 <?php foreach($ta as $th) { foreach($th as $t) { ?>
-                                                <option value="<?=$t->id?>"><?=$t->tahun_akademik?></option>
+                                                <option value="<?=$t->id?>"><?=$t->tahun_akademik ."-".$t->app_id?></option>
                                                 <?php }}?>
                                             </select>
                                         </div>
